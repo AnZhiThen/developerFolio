95caffc809d48534c6c92157fadd75d98fcc152d
Then An Zhi <xiaoturtle93@gmail.com>
2020-11-09 22:46:45 +0800

    Updates

---
+++ precache-manifest.e4476f3365c0e1190de39d5ca3f60f33.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311591147bc3210fc8c739c6f931d480",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "371154f69cdc350c3af0",
+    "url": "/developerFolio/static/css/3.ff3172a2.chunk.css"
+  },
+  {
+    "revision": "0258dacda2c6b9a45f78",
+    "url": "/developerFolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "49b44447a404229d7d94",
+    "url": "/developerFolio/static/css/main.e6d22568.chunk.css"
+  },
+  {
+    "revision": "f455c2743819a974fa06",
+    "url": "/developerFolio/static/js/2.74005f72.chunk.js"
+  },
+  {
+    "revision": "371154f69cdc350c3af0",
+    "url": "/developerFolio/static/js/3.64ac390b.chunk.js"
+  },
+  {
+    "revision": "0258dacda2c6b9a45f78",
+    "url": "/developerFolio/static/js/4.3f9d3e86.chunk.js"
+  },
+  {
+    "revision": "49b44447a404229d7d94",
+    "url": "/developerFolio/static/js/main.6cbcea7f.chunk.js"
+  },
+  {
+    "revision": "f3c1554151535d7cfc02",
+    "url": "/developerFolio/static/js/runtime-main.fd511e2f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "2efa3b625f735d893d972aa3d249d00a",
+    "url": "/developerFolio/static/media/IMG_2401.2efa3b62.JPG"
+  },
+  {
+    "revision": "14d1275c67676cc5d911232d0c890d97",
+    "url": "/developerFolio/static/media/Inter-Regular.14d1275c.woff"
+  },
+  {
+    "revision": "b51682264c374486f887b6069d68b5ab",
+    "url": "/developerFolio/static/media/Lomesty.b5168226.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/developerFolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "0b9bd815d1fc0fe39c25b8f1bd8eddd7",
+    "url": "/developerFolio/static/media/az-dp.0b9bd815.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "458a0bb242c79aecfcc780060829635b",
+    "url": "/developerFolio/static/media/javascriptComputer.458a0bb2.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "4b6b640e03f08c65724d87b6d9ed3df7",
+    "url": "/developerFolio/static/media/manCoding.4b6b640e.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "e7be8bccf8870cca484221fde59a8908",
+    "url": "/developerFolio/static/media/ntuLogo.e7be8bcc.png"
+  },
+  {
+    "revision": "9404c40fb0736e41c87ffb0181a39972",
+    "url": "/developerFolio/static/media/nypLogo.9404c40f.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "74ed1e3b252c73a38b7abde62ee30a64",
+    "url": "/developerFolio/static/media/skill.74ed1e3b.svg"
+  },
+  {
+    "revision": "b95eef6e6bd2733a5ffd13adcf0de480",
+    "url": "/developerFolio/static/media/talksCardBack.b95eef6e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a013bb8bf50572e543614363dc637678.js"
+  "/developerFolio/precache-manifest.e4476f3365c0e1190de39d5ca3f60f33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
